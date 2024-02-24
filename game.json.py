a102b116509375d6d39d626e53a6560886c2df4b
JackVM2001 <156926086+JackVM2001@users.noreply.github.com>
2024-02-23 20:48:10 -0500

    game.json.py

--- game.json.py
+++ game.json.py
@@ -5,4 +5,33 @@ Created on Wed Feb 21 17:55:57 2024
 
 @author: carlgroundstine
 """
-
+{
+  "start": [
+    "win or lose",
+    "win",
+    "win",
+    "lose",
+    "lose"
+  ],
+  "win": [
+    "You win",
+    "start over",
+    "start",
+    "quit",
+    "quit"
+  ],
+  "lose": [
+    "You lose",
+    "start over",
+    "start",
+    "quit",
+    "quit"
+  ],
+  "fr": [
+    "fr",
+    "fr",
+    "rf",
+    "rf",
+    "rf"
+  ]
+}
